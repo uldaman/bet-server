39024a6920e104acdca1326e0fce8d8790baa02c
hanxiao <zhuec@hotmail.com>
2019-04-01 13:30:52 +0800

    fix: mysql has gone away

--- src/app.py
+++ src/app.py
@@ -1,11 +1,25 @@
-# -*- coding:utf-8 -*-
 from api_quiz import QuizsAPI, QuizAPI
 from api_player import PlayerAPI
 from flask import Flask
 from flask_restful import Api
 from flask_cors import CORS
+from repository import mysql_db
+
 
 app = Flask(__name__, static_url_path='/static')
+
+
+@app.before_request
+def _db_connect():
+    mysql_db.connect(reuse_if_open=True)
+
+
+@app.teardown_request
+def _db_close(exc):
+    if not mysql_db.is_closed():
+        mysql_db.close()
+
+
 api = Api(app)
 CORS(app, supports_credentials=True)
 
--- src/repository/__init__.py
+++ src/repository/__init__.py
@@ -4,5 +4,6 @@ import os
 sys.path.append(os.path.split(os.path.realpath(__file__))[0])
 
 
+from base_model import mysql_db  # noqa
 from quiz import Quiz  # noqa
 from player import Player  # noqa
